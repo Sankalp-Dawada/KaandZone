249bf5a5b5e30ba59b7c2ac3f03e39dad7dbcdba
Sankalp-Dawada <sankalpdawada@gmail.com>
2025-05-27 22:59:13 +0530

    fix app and make it responsive

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000 // in KB, e.g., 1000 KB = 1 MB
+  }
 })
